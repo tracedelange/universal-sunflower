e7f661e219151a4f73802ff2d98f9a14973de7ac
Trace <tracedelange@me.com>
2021-06-02 12:35:51 -0500

    Added images, reduced errors

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 
-function addClickEventListener(element) {
-    element.addEventListener('click', function() {
+
+function addClickEventListener(input) {
+    input.addEventListener('click', function() {
         alert("Ahh you called my bluff! These links will come later when my webpage is expanded to webpage(s).")
     })
 }
 
-
-const links = document.getElementsByClassName("nav-link")
-
-for (link in links) {
-    addClickEventListener(links[link])
+function getRandomImage() {
+    const randomNumber = Math.floor(Math.random() * 7);
+    exampleImage.src = `images/${randomNumber + 1}.jpg`
 }
+
+const links = document.getElementsByClassName("nav-link");
+
+for (let i = 0; i < links.length; i++) {
+    addClickEventListener(links[i])
+}
+
+const exampleImage = document.getElementById("software-images")
+
+
+
+exampleImage.addEventListener('click', function(){
+    getRandomImage()
+})
 
