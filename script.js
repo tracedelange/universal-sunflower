11496fb06158c81e4b357b2687f9a07de91cc172
Trace <tracedelange@me.com>
2021-06-02 11:55:03 -0500

    Forgot to link js to html, event listeners working properly

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-/*
+
 function addClickEventListener(element) {
-    element.addClickEventListener('click', function() {
-        alert("Ahh you called my bluff! These links will come later.")
+    element.addEventListener('click', function() {
+        alert("Ahh you called my bluff! These links will come later when my webpage is expanded to webpage(s).")
     })
 }
+
 
 const links = document.getElementsByClassName("nav-link")
 
 for (link in links) {
     addClickEventListener(links[link])
 }
-*/
+
